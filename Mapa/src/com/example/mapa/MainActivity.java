29eb140575b0e4e97d7d90f59ed2f54d54c97c54
Rodrigo <rcastro87@gmail.com>
2014-04-01 12:37:29 -0400

    --> Mapa centrado en posicion actual, independiente del proveedor de localizacion (GPS o red)

--- Mapa/src/com/example/mapa/MainActivity.java
+++ Mapa/src/com/example/mapa/MainActivity.java
@@ -1,35 +1,49 @@
 package com.example.mapa;
-//??
+
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
-import android.app.Activity;
+
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.Toast;
 
-public class MainActivity extends android.support.v4.app.FragmentActivity {
+public class MainActivity extends android.support.v4.app.FragmentActivity implements android.location.LocationListener {
 
 	GoogleMap mapa;
-	
+	LocationClient mLocationClient;
+	LocationManager handle;
+	private String provider;
+	LatLng ubicacion;
+	ProgressDialog pd;
+	Location loc;
+		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-					
+		
 		mapa = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();	//Inicializamos el objeto mapara para poder manipular
+		mapa.setMapType(GoogleMap.MAP_TYPE_NORMAL);													//Seleccionamos el tipo de mapa de acuerdo a lo requeridoMAP_TYPE_NORMAL / MAP_TYPE_HYBRID / MAP_TYPE_SATELLITE / MAP_TYPE_TERRAIN
 		
-		mapa.setMapType(GoogleMap.MAP_TYPE_TERRAIN);													//Seleccionamos el tipo de mapa de acuerdo a lo requeridoMAP_TYPE_NORMAL / MAP_TYPE_HYBRID / MAP_TYPE_SATELLITE / MAP_TYPE_TERRAIN
-		mostrarMarcador(-33.4547699,-70.64989889999998);
+		mapa.setMyLocationEnabled(true);
 		
+		centrarMapa();	
+		mostrarMarcador(ubicacion);
 		mostrarLineas();
 		
 		mapa.setOnMarkerClickListener(new OnMarkerClickListener() {
@@ -42,16 +56,50 @@ public class MainActivity extends android.support.v4.app.FragmentActivity {
 		});
 	}
 	
-	private void mostrarMarcador(double lat, double lng)
+	public void centrarMapa(){
+			        
+	    handle = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+	    Criteria c = new Criteria();
+	    c.setAccuracy(Criteria.ACCURACY_FINE);
+	    c.setAltitudeRequired(false);
+	    //obtiene el mejor proveedor en función del criterio asignado
+	    //(PRESISION MEDIA)
+	    provider = handle.getBestProvider(c, true);
+	    //Se activan las notificaciones de localización con los parámetros: 
+	    //proveedor, tiempo mínimo de actualización, distancia mínima, Locationlistener
+	    handle.requestLocationUpdates(provider, 1000, 1, this);
+	    loc = handle.getLastKnownLocation(provider);
+	    
+	    if(loc != null){
+		    //Obtenemos la última posición conocida dada por el proveedor
+		    ubicacion = new LatLng(loc.getLatitude(),loc.getLongitude());
+			configGPS(ubicacion);
+		}else
+		{	
+			ubicacion = new LatLng(-33.442909,-70.65386999999998);
+			Toast t1 = Toast.makeText(this, "No se pudo Obtener la ubicacion", Toast.LENGTH_LONG);
+			t1.show();
+			configGPS(ubicacion);
+		 }		
+	}
+	private void configGPS(LatLng latlong) {
+		
+		// TODO Auto-generated method stub
+		//ubicacion = new LatLng(loc.getLatitude(),loc.getLongitude());
+		CameraPosition cameraPosition = new CameraPosition.Builder().target(latlong).zoom(12).build();	
+		mapa.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+	}
+
+	private void mostrarMarcador(LatLng latlong)
 	{
-	    mapa.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title("Usted esta aqui"));	//title(para el titulo) snippet(para el resto del texto) 
+		//ubicacion = new LatLng(loc.getLatitude(),loc.getLongitude());	
+	    mapa.addMarker(new MarkerOptions().position(latlong).title("Usted esta aqui"));	//title(para el titulo) snippet(para el resto del texto)
 	}
 	
 	private void mostrarLineas()
 	{
 	    //Dibujo con Lineas
-	 
-	    PolylineOptions lineas = new PolylineOptions()
+		PolylineOptions lineas = new PolylineOptions()
 	            .add(new LatLng(45.0, -12.0))
 	            .add(new LatLng(45.0, 5.0))
 	            .add(new LatLng(34.5, 5.0))
@@ -69,5 +117,29 @@ public class MainActivity extends android.support.v4.app.FragmentActivity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-}
 
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+		ubicacion = new LatLng(loc.getLatitude(),loc.getLongitude());
+		configGPS(ubicacion);
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
+}
